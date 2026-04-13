bbdf4a984b587058308a0d28debd1188f279b7d4
LourdesDomi <lourdesdominguez350@gmail.com>
2026-04-13 18:54:14 -0300

    Update CuentaService.java

--- Servicio/CuentaService.java
+++ Servicio/CuentaService.java
@@ -1,1 +1,17 @@
+package servicio;
 
+import model.CuentaCorriente;
+import java.util.List;
+
+public interface CuentaService {
+
+    void agregarCuenta(CuentaCorriente cuenta);
+
+    CuentaCorriente buscarCuenta(String numero);
+
+    boolean eliminarCuenta(String numero);
+
+    List<CuentaCorriente> listarCuentas();
+
+    void depositar(String numero, double monto);
+}
