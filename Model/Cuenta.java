1da18d1db56099e24537cd941356976386f73725
LourdesDomi <lourdesdominguez350@gmail.com>
2026-04-13 18:42:19 -0300

    Update Cuenta.java

--- Model/Cuenta.java
+++ Model/Cuenta.java
@@ -1,1 +1,50 @@
+package model;
 
+public class Cuenta {
+
+    private String titular;
+    private String numeroCuenta;
+    protected double saldo;
+
+    public Cuenta(String titular, String numeroCuenta, double saldo) {
+        this.titular = titular;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double monto) {
+        saldo += monto;
+    }
+
+    public boolean extraer(double monto) {
+        if (saldo >= monto) {
+            saldo -= monto;
+            return true;
+        }
+        return false;
+    }
+}
